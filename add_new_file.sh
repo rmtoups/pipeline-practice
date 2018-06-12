7a966793640dc1b869dc13e3e25829a171e1c555
rmtoups <rmtoups@gmail.com>
2018-06-12 10:59:31 -0400

    fixed stuff

--- add_new_file.sh
+++ add_new_file.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 cd updated-github
+
+git config --global user.email "rmtoups@gmail.com"
+git config --global user.name "rmtoups"
+
 git add *.txt
 git commit -m "added new text file"
--- clone_repo.sh
+++ clone_repo.sh
@@ -4,5 +4,3 @@ git clone rmtoups-github updated-github
 
 cd updated-github
 
-git config --global user.email "rmtoups@gmail.com"
-git config --global user.name "rmtoups"
