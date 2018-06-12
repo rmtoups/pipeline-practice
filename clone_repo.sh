3e9d7715928235b037367bb09794b76d4df2eb51
rmtoups <rmtoups@gmail.com>
2018-06-12 14:50:41 -0400

    um

--- clone_repo.sh
+++ clone_repo.sh
@@ -3,6 +3,6 @@
 git clone rmtoups-github updated-github
 
 cd updated-github
-cd rmtoups-github
+#cd rmtoups-github
 ls
 
--- create-file.py
+++ create-file.py
@@ -3,9 +3,16 @@
 import sys
 import os
 
+os.listdir()
+
 os.chdir("rmtoups-github")
+
+os.getcwd()
 
 file = open("file_"+sys.argv[1]+".txt",'w+')
 file.write("hello?")
 file.close()
+
+os.listdir()
+
 print("hello")
