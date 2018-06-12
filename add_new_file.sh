867428c08dd3452ed7fb532fe0cc482842657d82
rmtoups <rmtoups@gmail.com>
2018-06-12 16:03:51 -0400

    committing add_new_file.sh

--- add_new_file.sh
+++ add_new_file.sh
@@ -13,7 +13,7 @@ ls
 git config --global user.email "rmtoups@gmail.com"
 git config --global user.name "rmtoups"
 
-git add *.txt
+git add .
 git status
 git commit -m "added new text file"
 #git push
